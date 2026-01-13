99aa2715be1059238fc96358cc01480ba1fdb9c6
dr-george-dev <labegeorgeedu@gmail.com>
2026-01-13 20:22:36 +0100

    Session and cookies create, i'll use it in the auth.ts

--- src/app/_lib/session.ts
+++ src/app/_lib/session.ts
@@ -1,5 +1,31 @@
 //Set session cookie
 
+import { cookies } from "next/headers";
+import { UserType } from "../_types/user";
+
+export const setSession = async (user: UserType) => {
+    (await cookies()).set("session", JSON.stringify(user), {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 60 * 60 * 24 * 7, // 1 week
+        path: "/",
+    });
+ 
+};
+
+
 //Get session cookie
+export const getSession = async (): Promise<UserType | null> => {
+   const session = (await cookies()).get("session")?.value;
+if (!session) return null;
+const user = JSON.parse(session) as UserType;
+return user; 
+ 
+};
+
 
 //delete session cookie
+export const deleteSession = async () => {
+    const cookieStore = await cookies();
+    cookieStore.delete("session");
+};
