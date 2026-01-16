70083c3f9501493c96882d70c33bec9bbffdad7a
dr-george-dev <labegeorgeedu@gmail.com>
2026-01-16 10:07:24 +0100

    update new, edit contact page

--- src/app/_components/ContactForm.tsx
+++ src/app/_components/ContactForm.tsx
@@ -1,60 +1,65 @@
-import React, { useActionState } from 'react'
-import { ContactType } from '../_types/contact';
-
+"use client";
+import React, { useActionState, useEffect } from "react";
+import { ContactType } from "../_types/contact";
+import { Router, useRouter } from "next/router";
 
 type ContactFormProps = {
   action: (prevState: any, formData: FormData) => Promise<any>;
   contact?: ContactType;
 };
 
-const ContactForm = ({action, contact}: ContactFormProps) => {
+const ContactForm = ({ action, contact }: ContactFormProps) => {
+  const router = useRouter();
   const [state, formAction] = useActionState(action, null);
+
+  useEffect(() => {
+    if (state?.success) {
+      router.push("/contact");
+    }
+  }, [state, router]);
 
   return (
     <form action={formAction} className="space-x-4">
-      
-      {/* 3. This block displays the error message from the server */}
-      {state?.message && (
-        <div className="mb-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded">
-          {state.message}
-        </div>
-      )}
-
       <div className="">
-        <label className="block text-sm font-medium text-gray-700">
-          Email
+        <label
+          htmlFor="name"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Name
         </label>
-        <input 
-          type="email"
-          name="email"
-          placeholder='Enter your email'
-          required
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your name"
           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
         />
       </div>
 
       <div className="mt-3">
-        <label className="block text-sm font-medium text-gray-700">
-          Password
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Email
         </label>
-        <input 
-          type="password"
-          name="password"
-          placeholder='Enter your password'
-          required
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2"
         />
       </div>
-
+      {state?.error && (
+        <div className="text-red-500 text-sm">{state.error}</div>
+      )}
       <button
-        type='submit'
-        disabled={isPending}
-        className={`mt-3 w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white 
-        ${isPending ? 'bg-blue-400 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-700'}`}>
-          {isPending ? "Logging in..." : "Login"}
+        type="submit"
+        className="mt-3 w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-blue-600 hover:bg-blue-700"
+      >
+        Save Contact
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
--- src/app/actions/contact.ts
+++ src/app/actions/contact.ts
@@ -1,18 +1,42 @@
 "use server"
 import { revalidatePath } from "next/cache";
-import { deleteContact } from "../api/contact";
+import { craeteContact, deleteContact } from "../api/contact";
+import { getSession } from "../_lib/session";
+import { ContactType } from "../_types/contact";
 
 
 //  These are all mutations, time 28:00
 export const createContactAction = async (
     prevState: any,
     formData: FormData
-) => {};
+) => {
+    if (!formData) {
+        return { error: `Form data is missing` };
+    }
+
+    const user = await getSession();
+
+    const newContact: ContactType = {
+        name: formData.get("name") as string,
+        email: formData.get("email") as string,
+        usreId: user?.id
+    };
+    try {
+        await craeteContact(newContact);
+        revalidatePath("/contact");
+        return { success: true };
+    } catch (error) {
+        console.log("Error create contact: ", error);
+        return {
+            error: "Failed to create contact"
+        };
+    }
+};
 
 export const updateContactAction = async (
     prevState: any,
     formData: FormData
-) => {};
+) => { };
 
 export const deletContactAction = async (
     prevState: any,
@@ -27,7 +51,7 @@ export const deletContactAction = async (
     } catch (error) {
         console.log("Error deleting contact: ", error);
         return {
-            error: "Failed t delete contact"
+            error: "Failed to delete contact"
         };
     }
 };
